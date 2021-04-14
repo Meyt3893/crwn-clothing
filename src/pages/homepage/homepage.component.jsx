354c367c171575d3070cb07ba360b2cb5b38de86
Your Name <you@example.com>
2021-04-14 10:14:06 +0300

    preview collection

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,7 +3,7 @@ import './homepage.styles.scss'
 import Directory from '../../components/directory/directory.component'
 
 const HomePage =() => (
-    <div cclassName='homepage'>
+    <div className='homepage'>
         <Directory/>
     </div>
 );
